e0f968cace1d0835bb79bb0f345066a2d8072314
WKingDeveloper <wkingdeveloper@gmail.com>
2023-06-30 08:37:17 +0900

    chore : dgs 라이브러리 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ repositories {
 }
 
 dependencies {
+    implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release"))
+    implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
